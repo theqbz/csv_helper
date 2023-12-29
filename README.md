64fa9621b15e3537bbba34f477c5ced33f4b1e33
QBZ <borsodi.zoltan@gmail.com>
2023-12-29 20:27:37 +0100

    Added configHelp and update Readme

--- README.md
+++ README.md
@@ -1,27 +1,42 @@
 # CSV Validator
 
-**This program can analyze the provided csv files, seeking for errors. It can detect Records that occur more than once and if a Record contains more or less Fileds than the number of Labels. (The Analyzer identifies Label list in the first non empty row of the file!)**
+**This program can analyze the provided csv files, seeking for errors. It can
+detect Records that occur more than once and if a Record contains more or less
+Fileds than the number of Labels. (The Analyzer identifies Label list in the
+first non empty row of the file!)**
 
 To parse a file, start the program with a filename or a path:
 
         csv_validator.exe  path/or/file  [settings]
 
-To get help, use the `help` command!
-
 To sore user's settings permanently, use `config` command!
+
+To get help, use the `help` command!
 
 ## Settings:
 
-| arg            | type     | possible values             | description                                                |
-|----------------|----------|-----------------------------|------------------------------------------------------------|
-| `-delimiter`   | (char)   | default value: `;`          | the delimiter between Fields in .csv file                  | 
-| `-emptyLines`  | (string) | `skip`* / `error` / `leave` | defines what to do with empty lines                        |
-| `-emptyFields` | (char)   | default value: `_`          | placeholder for empty values; <empty>: skip empty values   |
-| `-label`       | (string) | `top`* / `inline`           | defines where to print the labels                          |
-| `-errorLines`  | (int)    | default value: `0`          | the number of lines showed around errors (`-1`: all lines) |
-| `-errorLevel`  | (string) | `all` / `error`*            | defines the verbosity of the error log                     |
+| arg            | type   | possible values             | description                                                |
+|----------------|--------|-----------------------------|------------------------------------------------------------|
+| `-delimiter`   | char   | default value: `;`          | the delimiter between Fields in .csv file                  | 
+| `-emptyLines`  | string | `skip`* / `error` / `leave` | defines what to do with empty lines                        |
+| `-emptyFields` | char   | default value: `_`          | placeholder for empty values; <empty>: skip empty values   |
+| `-label`       | string | `top`* / `inline`           | defines where to print the labels                          |
+| `-errorLines`  | int    | default value: `0`          | the number of lines showed around errors (`-1`: all lines) |
+| `-errorLevel`  | string | `all` / `error`*            | defines the verbosity of the error log                     |
 
-*\* default values*
+*\* default value*
+
+## Config
+
+The program can permanently store user's own settings in an .ini file. To use
+this function start the program with the `config` command and provide the
+settings to be store:
+
+        csv_validator.exe  config  [arguments: setting's name and value]
+
+Multiple arguments can be specified. The `config` command with no arguments
+creates a new .ini file with the default settings (or overwrites the existing
+one).
 
 ## Libraries
 
--- src/task/HelpTask.cpp
+++ src/task/HelpTask.cpp
@@ -16,6 +16,8 @@ namespace task {
 
 static std::string aboutProgram();
 static std::string aboutStart();
+static std::string aboutSettings();
+static std::string aboutConfig();
 static std::string aboutHelp();
 
 bool HelpTask::run()
@@ -42,6 +44,8 @@ void HelpTask::printShortHelp() const
     std::cout << "No file or command presented.\n"
               << aboutProgram()
               << aboutStart()
+              << "\n"
+              << aboutConfig()
               << aboutHelp()
               << "\n";
 }
@@ -51,20 +55,10 @@ void HelpTask::printFullHelp() const
     DEBUG_LOG("Printing full help\n\n", utils::verbose);
     std::cout //
         << aboutStart()
+        << aboutSettings()
         << "\n"
-        << utils::INDENTATION + "Settings:\n"
-        << "\n"
-        << utils::INDENTATION + "-delimiter (char)  -  default value: \";\" (the delimiter between Fields in .csv file)\n"
-        << "\n"
-        << utils::INDENTATION + "-emptyLines (string) [ skip | error | leave ]  -  default value: \"skip\" (defines what to do with empty lines)\n"
-        << "\n"
-        << utils::INDENTATION + "-emptyFields (char)  -  default value: \".\" (placeholder for empty values; <empty>: skip empty values)\n"
-        << "\n"
-        << utils::INDENTATION + "-label (string) [ top | inline ]  -  default value: \"top\" (defines where to print the labels)\n"
-        << "\n"
-        << utils::INDENTATION + "-errorLines (int)  -  default value: \"0\" (the number of lines around errors; -1: all lines)\n"
-        << "\n"
-        << utils::INDENTATION + "-errorLevel (string) [ all | error ]  -  default value: \"error\" (defines the verbosity of the error log)\n"
+        << aboutConfig()
+        << aboutHelp()
         << "\n";
 }
 
@@ -72,10 +66,17 @@ void HelpTask::printSettingWriterHelp() const
 {
     DEBUG_LOG("Printing config writer help\n\n", utils::verbose);
     std::cout << "\n"
-              << "Setting writer help... soon.\n"
+              << utils::INDENTATION + "The program can permanently store user's own settings in an .ini file.\n"
+              << utils::INDENTATION + "To use this function start the program with the  config  command and\n"
+              << utils::INDENTATION + "provide the settings to be store:\n"
+              << "\n"
+              << utils::INDENTATION + "\t> csv_validator.exe  config  [arguments: setting's name and value]\n"
+              << "\n"
+              << utils::INDENTATION + "Multiple arguments can be specified.\n"
+              << utils::INDENTATION + "The  config  command with no arguments creates a new .ini file with\n"
+              << utils::INDENTATION + "the default settings (or overwrites the existing one).\n"
+              << aboutSettings()
               << "\n";
-    // TODO:
-    // Write the setting writer instructions
 }
 
 std::string aboutProgram()
@@ -90,15 +91,42 @@ std::string aboutProgram()
 std::string aboutStart()
 {
     return "\n"
-        + utils::INDENTATION + "To parse a file start the program with a filename or a path:\n"
+        + utils::INDENTATION + "To parse a file, start the program with a filename or a path:\n"
                                "\n"
         + utils::INDENTATION + "\t>  csv_validator.exe  path/to/file  [settings]\n";
 }
 
-static std::string aboutHelp()
+std::string aboutSettings()
 {
     return "\n"
-        + utils::INDENTATION + "To get help, use the  help  command!\n";
+        + utils::INDENTATION + "Settings:\n"
+        + "\n"
+        + utils::INDENTATION + "-delimiter (char)  -  default value: \";\" (the delimiter between Fields in .csv file)\n"
+        + "\n"
+        + utils::INDENTATION + "-emptyLines (string) [ skip | error | leave ]  -  default value: \"skip\" (defines what to do with empty lines)\n"
+        + "\n"
+        + utils::INDENTATION + "-emptyFields (char)  -  default value: \".\" (placeholder for empty values; <empty>: skip empty values)\n"
+        + "\n"
+        + utils::INDENTATION + "-label (string) [ top | inline ]  -  default value: \"top\" (defines where to print the labels)\n"
+        + "\n"
+        + utils::INDENTATION + "-errorLines (int)  -  default value: \"0\" (the number of lines around errors; -1: all lines)\n"
+        + "\n"
+        + utils::INDENTATION + "-errorLevel (string) [ all | error ]  -  default value: \"error\" (defines the verbosity of the error log)\n"
+        + "\n";
+}
+
+std::string aboutConfig()
+{
+    return "\n"
+        + utils::INDENTATION + "To store user's settings permanently, use the  config  command!\n"
+        + "\n";
+}
+
+std::string aboutHelp()
+{
+    return "\n"
+        + utils::INDENTATION + "To get help, use the  help  command!\n"
+        + "\n";
 }
 
 } // namespace task
